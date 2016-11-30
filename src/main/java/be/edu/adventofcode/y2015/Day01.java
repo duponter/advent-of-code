8f15fbef67c76b1306db498d97f13d6fbd9093ff
Erwin Dupont <erwin.dupont@aca-it.be>
2016-11-30 22:52:15 +0100

    Advent 2015 Day 01 Part 2

--- src/main/java/be/edu/adventofcode/y2015/Day01.java
+++ src/main/java/be/edu/adventofcode/y2015/Day01.java
@@ -7,19 +7,33 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import javaslang.collection.CharSeq;
+
 import org.apache.commons.lang3.StringUtils;
 
 public class Day01 {
 
     public static void main(String[] args) {
         new Day01().part1();
+        new Day01().part2();
     }
 
     private void part1() {
         String input = this.readInput();
         int up = StringUtils.countMatches(input, '(');
         int down = StringUtils.countMatches(input, ')');
-        System.out.printf("Going up %d times, going down %d times, ending at floor %d", up, down, up - down);
+        System.out.printf("Going up %d times, going down %d times, ending at floor %d%n", up, down, up - down);
+
+        System.out.printf("Day 1 - Part 1 - To what floor do the instructions take Santa? %d%n", CharSeq.of(input)
+                .foldLeft(0, (pos, next) -> (pos += (next == '(') ? 1 : -1))
+        );
+    }
+
+    private void part2() {
+        System.out.printf("Day 1 - Part 2 - What is the position of the character that causes Santa to first enter the basement? %d%n", CharSeq.of(this.readInput())
+                .scanLeft(0, (pos, next) -> (pos += (next == '(') ? 1 : -1))
+                .takeUntil(pos -> pos == -1)
+                .size());
     }
 
     private String readInput() {
