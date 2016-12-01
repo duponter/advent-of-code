e897d902d0d114be4e9694e21b8e5ff46947d228
Erwin Dupont <erwin.dupont@aca-it.be>
2016-12-01 20:58:16 +0100

    Advent 2015 Day 02 stubs

---
+++ src/main/java/be/edu/adventofcode/y2015/Day02.java
@@ -0,0 +1,16 @@
+package be.edu.adventofcode.y2015;
+
+public class Day02 {
+    public static void main(String[] args) {
+        new Day02().part1();
+        new Day02().part2();
+    }
+
+    private void part1() {
+        System.out.printf("Day 2 - Part 1 - %n");
+    }
+
+    private void part2() {
+        System.out.printf("Day 2 - Part 2 - %n");
+    }
+}
