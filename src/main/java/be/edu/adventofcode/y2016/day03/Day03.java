d2cfcee99113aa242ea88480da6a66d665edd1f8
Erwin Dupont <erwin.dupont@aca-it.be>
2016-12-05 21:34:57 +0100

    Advent 2016 Day 03 Part 2

--- src/main/java/be/edu/adventofcode/y2016/day03/Day03.java
+++ src/main/java/be/edu/adventofcode/y2016/day03/Day03.java
@@ -1,5 +1,7 @@
 package be.edu.adventofcode.y2016.day03;
 
+import javaslang.collection.Array;
+
 import be.edu.adventofcode.Input;
 
 public class Day03 {
@@ -10,8 +12,19 @@ public class Day03 {
     }
 
     public int part2(Input input) {
-        return input.lines()
+        Array<String[]> lines = input.lines()
+                .map(line -> line.trim().split("\\s+"))
+                .toArray();
+        return transpose(lines)
+                .reduce(Array::appendAll)
+                .grouped(3)
+                .map(tokens -> String.join(" ", tokens))
                 .map(Triangle::parse)
                 .count(triangle -> !triangle.isInequal());
+    }
+
+    private <T> Array<Array<T>> transpose(Array<T[]> a) {
+        return Array.tabulate(a.get().length, row ->
+                Array.tabulate(a.size(), col -> a.get(col)[row]));
     }
 }
