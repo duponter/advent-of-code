61d08acd0683ce9a2260c916b2009c61151b8b28
Erwin Dupont <erwin.dupont@aca-it.be>
2016-12-15 23:00:23 +0100

    Advent 2016 Day 11 setup

---
+++ src/main/java/be/edu/adventofcode/y2016/day11/Day11.java
@@ -0,0 +1,13 @@
+package be.edu.adventofcode.y2016.day11;
+
+import be.edu.adventofcode.Input;
+
+public class Day11 {
+    public int part1(Input input) {
+        return 0;
+    }
+
+    public int part2(Input input) {
+        return 0;
+    }
+}
